c02c51d75c9bc004bcfdf15a6c46f449a09232d4
Sadie <sfreisthler@gmail.com>
2023-04-11 00:36:42 -0400

    Added necessary project files

---
+++ src/kilo.c
@@ -0,0 +1,43 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+struct termios orig_termios;
+
+void disableRawMode() {
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void enableRawMode() {
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(disableRawMode);
+
+    struct termios raw = orig_termios;
+
+    // ICANON: turns off canonical mode, read input byte 
+    // by byte instead of line-by-line
+    // ISIG diables ^C and ^X
+    // IXON diables ^S and ^Q
+    raw.c_iflag &= ~(ICRNL | IXON);
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+
+}
+
+int main() {
+    enableRawMode();
+
+    
+    char c;
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+	if (iscntrl(c)) {
+	    printf("%d\n",c);
+	} else {
+	    printf("%d ('%c')\n", c, c);
+	}
+    }
+    return EXIT_SUCCESS;
+}
