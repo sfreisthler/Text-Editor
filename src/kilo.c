0aa73cda49d339b706553ef253b6a2ef7c6b2f7a
Sadie <sfreisthler@gmail.com>
2023-04-11 00:45:04 -0400

    Added timeout for read()

--- src/kilo.c
+++ src/kilo.c
@@ -7,6 +7,7 @@
 struct termios orig_termios;
 
 void disableRawMode() {
+    // Reset to canonical mode atexit
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
 }
 
@@ -20,8 +21,14 @@ void enableRawMode() {
     // by byte instead of line-by-line
     // ISIG diables ^C and ^X
     // IXON diables ^S and ^Q
-    raw.c_iflag &= ~(ICRNL | IXON);
+    raw.c_iflag &= ~(BRKINT | INPCK | ISTRIP | ICRNL | IXON);
+    // turn off all output processing features
+    raw.c_oflag &= ~(OPOST);
+    raw.c_cflag |= (CS8);
     raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
 
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 
@@ -31,13 +38,16 @@ int main() {
     enableRawMode();
 
     
-    char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+    // Read byte-by-byte from stdin
+    while (1) {
+	char c = '\0';
+	read(STDIN_FILENO, &c, 1);
 	if (iscntrl(c)) {
-	    printf("%d\n",c);
+	    printf("%d\r\n",c);
 	} else {
-	    printf("%d ('%c')\n", c, c);
+	    printf("%d ('%c')\r\n",c,c);
 	}
+	if (c == 'q') break;
     }
     return EXIT_SUCCESS;
 }
